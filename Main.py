36a5cf157205a32caac702566aa26c7e847da20d
GNIJZ <1006631645@qq.com>
2024-05-28 15:20:26 +0800

    # Conflicts: #	.idea/workspace.xml

--- Main.py
+++ Main.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
         audio_recorder = AudioRecorder(channels=1, sample_rate=16000)
         transcriber = Transcriber(model_size="Whisperme/model/largev3/")
         classifier = pipeline("text-classification", model='Whisperme/model/bert_emotion', top_k=0)
-        speech_thread = SpeechThread(transcriber, audio_recorder,classifier,True)
+        # speech_thread = SpeechThread(transcriber, audio_recorder,classifier,True)
+        speech_thread = SpeechThread(transcriber, audio_recorder,True)
         # if message is not None:
         #     chatspeak(message,model="Llm/model/llama/",file="Llm/temp_audio.mp3")
         manager0 = ThreadManager(function=speech_thread)
@@ -28,13 +29,11 @@ if __name__ == '__main__':
     except Exception as e:
         logging.error(e, exc_info=True)
 
-    # mpFaceMesh = mp.solutions.face_mesh
-    # face_mesh = mpFaceMesh.FaceMesh(static_image_mode=False,
-    #                                 max_num_faces=1,
-    #                                 min_detection_confidence=0.5,
-    #                                 min_tracking_confidence=0.5)
-    # mediapipe_thread=MediapipeThread(face_mesh)
-    # manager1 = ThreadManager(function=mediapipe_thread)
-    # manager1.control_thread(True)
-    # message="hello"
-    # chatspeak(message,model="Llm/llama/",file="Llm/temp_audio.mp3")
+    mpFaceMesh = mp.solutions.face_mesh
+    face_mesh = mpFaceMesh.FaceMesh(static_image_mode=False,
+                                    max_num_faces=1,
+                                    min_detection_confidence=0.5,
+                                    min_tracking_confidence=0.5)
+    mediapipe_thread=MediapipeThread(face_mesh)
+    manager1 = ThreadManager(function=mediapipe_thread)
+    manager1.control_thread(True)
